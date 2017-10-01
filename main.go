bdc61c5ef14c265caf6fc60d1a6192057a693574
Max J. Schridde <maxjschridde@gmail.com>
2017-09-30 18:12:43 -0700

    Added basic restserver with two routes: 1) /blocks 2) /add

--- main.go
+++ main.go
@@ -1,12 +1,7 @@
 package main
 
-import (
-	"blockchain/chain"
-)
+import "blockchain/restserver"
 
 func main() {
-	bc := chain.NewBlockchain()
-
-	bc.AddBlock("Send 1 BTC to Ivan")
-	bc.AddBlock("Send 2 more BTC to Ivan")
+	restserver.Setup()
 }
